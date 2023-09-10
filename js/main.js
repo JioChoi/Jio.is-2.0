e918ab6eb1540263a56fa3a7e0fc0630d7173622
JioChoi <jiochoi7@gmail.com>
2023-09-10 14:41:08 +0900

    "Who am I?" title 😘

--- js/main.js
+++ js/main.js
@@ -1,49 +1,146 @@
 window.onload = function () {
 	initTypeWriter();
 	
+	addEventListener("scroll", scrollAnimation);
+
 	// Hide loading screen
 	document.getElementById("loading").classList.add("hidden");
+}
+
+function scrollAnimation() {
+	var scroll = window.scrollY;
+	var height = window.innerHeight;
+
+	var pageHeight = (height / 2);
+	var page = Math.floor(scroll / pageHeight);
+	var transition = (scroll % pageHeight) / pageHeight;
+
+	// Intro
+	if (page == 0) {
+		document.getElementById("intro").style.opacity = 1 - transition;
+		document.getElementById("intro").style.transform = "translateY(" + (transition * -10) + "vh)";
+	}
+	else {
+		document.getElementById("intro").style.opacity = 0;
+		document.getElementById("intro").style.transform = "translateY(-10vh)";
+	}
+
+	// Who am I
+	if (page == 1) {
+		document.getElementById("whoami").style.opacity = transition * 2;
+		type(document.getElementById("whoami"), transition * 100);
+	}
+	else {
+		document.getElementById("whoami").style.opacity = 0;
+	}
+
+	// Who am I
+	if (page == 2) {
+		document.getElementById("whoami").style.opacity = 1;
+	}
+}
+
+function isOnScreen(element) {
+	var rect = element.getBoundingClientRect();
+	var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+	return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+
+function type(element, progress) {
+	var text = element.getAttribute("text").replace(/\n/g, "").replace(/\t/g, "");
+	var index = Math.floor((text.length + 1) * progress / 100);
+	element.innerHTML = text.substring(0, index) + "▋";
 }
 
 function initTypeWriter() {
 	var elements = document.getElementsByClassName("typewriter");
 	for (var i = 0; i < elements.length; i++) {
-		var text = elements[i].getAttribute("text").replace(/\n/g, "").replace(/\t/g, "").split(",");
-		typeWriter(elements[i], text, 0);
+		var element = elements[i];
+		var text = element.getAttribute("text").replace(/\n/g, "").replace(/\t/g, "").split("|");
+		var originalText = element.innerHTML;
+
+		if (text.length == 1) {
+			((ele, text, originalText) => {
+				waitUntilVisible(ele,
+					() => {
+						startTypeWriter(ele, text, 0);
+						console.log(ele);
+					}
+					, () => {
+						clearInterval(ele.interval);
+						ele.innerHTML = originalText;
+					}
+				);
+			})(element, text, originalText);
+		}
+		else {
+			startTypeWriter(element, text, 0);
+		}
 	}
 }
 
-function typeWriter(element, text, textIndex) {
+function waitUntilVisible(element, onShow, onHide) {
+	if (isOnScreen(element)) {
+		onShow();
+		waitUntilHidden(element, function () {
+			onHide();
+			waitUntilVisible(element, onShow, onHide);
+		});
+	} else {
+		setTimeout(() => {
+			waitUntilVisible(element, onShow, onHide);
+		}, 100);
+		return;
+	}
+}
+
+function waitUntilHidden(element, callback) {
+	if (!isOnScreen(element)) {
+		console.log("NOT ON SCREEN");
+		callback();
+	} else {
+		setTimeout(() => {
+			waitUntilHidden(element, callback);
+		}, 100);
+	}
+}
+
+function startTypeWriter(element, text, textIndex) {
 	const originalText = element.innerHTML;
 	var index = 0;
 
 	element.innerHTML = originalText + "▋";
 
-	const typeInterval = setInterval(() => {
+	element.interval = setInterval(() => {
 		/* Typing */
 		element.innerHTML = originalText + text[textIndex].substring(0, index) + "▋";
 		index++;
 
 		/* End of typing */
 		if (index > text[textIndex].length) {
-			clearInterval(typeInterval);
+			clearInterval(element.interval);
 
 			if (text.length != 1) {
 				setTimeout(() => {
 					/* Deleting */
-					const deleteInterval = setInterval(() => {
+					element.interval = setInterval(() => {
 						element.innerHTML = originalText + text[textIndex].substring(0, index) + "▋";
 						index--;
 
 						if (index < 0) {
-							clearInterval(deleteInterval);
+							clearInterval(element.interval);
 							element.innerHTML = originalText;
 							if (text.length != 1) {
-								typeWriter(element, text, (textIndex + 1) % text.length);
+								startTypeWriter(element, text, (textIndex + 1) % text.length);
 							}
 						}
 					}, 60);
 				}, 1000);
+			}
+			else {
+				setTimeout(() => {
+					element.innerHTML = originalText + text[textIndex];
+				}, 300);
 			}
 		}
 	}, 100);
