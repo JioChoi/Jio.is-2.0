defec5e6aae53ded41894a105420f578b5797c33
c5dingb5y <jiochoi7@gmail.com>
2023-10-01 12:42:21 +0900

    Particle!

--- js/main.js
+++ js/main.js
@@ -1,50 +1,43 @@
 var previousPage = 0;
 var page = 0;
 
-const skills = [
-	{type: "plang", word: "C++", size: 1},
-	{type: "lang", word: "English", size: 1},
-	{ type: "prog", word: "Photoshop", size: 1 },
-	
-	
-	{type: "plang", word: "JAVA", size: 2},
-	{type: "plang", word: "SDL2", size: 2},
-	{type: "plang", word: "C", size: 2},
-	{type: "plang", word: "HTML", size: 2},
-	{type: "plang", word: "CSS", size: 2},
-	{type: "plang", word: "JAVASCRIPT", size: 2},
-	{type: "plang", word: "Node.js", size: 2},
-	{type: "plang", word: "GDI+", size: 2},
-	{type: "plang", word: "Windows API", size: 2},
-	{type: "plang", word: "MFC", size: 2},
-	{type: "plang", word: "MATLAB", size: 2},
-	{type: "lang", word: "Japanese", size: 2},
-	{type: "lang", word: "Chinese", size: 2},
-	{type: "lang", word: "Korean", size: 2},
-	
-	{type: "plang", word: "IIS", size: 3},
-	{type: "plang", word: "Apache 2", size: 3},
-	{type: "plang", word: "MySQL", size: 3},
-	{type: "plang", word: "PHP", size: 3},
-	{type: "plang", word: "OpenGL", size: 3},
-	{type: "plang", word: "GLSL", size: 3},
-	{type: "plang", word: "Arduino", size: 3},
-	
-	{ type: "prog", word: "After Effects", size: 3 },
-	{ type: "prog", word: "Premiere Pro", size: 3 },
-	{ type: "prog", word: "Adobe XD", size: 3 },
-	{ type: "prog", word: "Ableton Live", size: 3 },
-	{ type: "prog", word: "FL Studio", size: 3 },
+var canvas;
+var ctx;
+var deltaEnd = 0;
+
+var particles;
+var particleSize = 5;
+
+var spawnInterval;
+
+const words = [
+	"C",
+	"C++",
+	"JAVA",
+	"SDL2",
+	"HTML",
+	"CSS",
+	"JAVASCRIPT",
+	"Node.js",
+	"GDI+",
+	"Windows API",
+	"MFC",
+	"MATLAB",
+	"IIS",
+	"Apache 2",
+	"MySQL",
+	"PHP",
+	"OpenGL",
+	"GLSL",
+	"Arduino"
 ];
 
-const word_offset_same = {x: 10, y: 10};
-const word_offset = {x: 10, y: 10};
+var createdParticles = [];
 
 window.onload = function () {
 	initTypeWriter();
 	initItems();
-
-	createOrbit();
+	initCanvas();
 
 	addEventListener("scroll", scrollAnimation);
 	scrollAnimation();
@@ -59,168 +52,150 @@ window.onload = function () {
 			document.getElementById("down").classList.add("animate");
 		}, 500);
 	}, 1000);
-}
 
-function getDistance(point1, point2) {
-	return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
-}
+	addEventListener("mousedown", function (e) {
+		var x = Math.floor(e.clientX / particleSize);
+		var y = Math.floor(e.clientY / particleSize);
 
-function createOrbit() {
-	var box = document.getElementById("wordbox");
-	var boxRect = box.getBoundingClientRect();
-
-	var center = { x: boxRect.width / 2, y: boxRect.height / 2 };
-	var placedWords = [];
-
-	for (var i = 0; i < skills.length; i++) {
-		var fontsize = 0;
-		switch (skills[i].size) {
-			case 1:
-				fontsize = 60;
-				break;
-			case 2:
-				fontsize = 40;
-				break;
-			case 3:
-				fontsize = 20;
-				break;
-		}
-
-		var x, y, distance = 0;
-		var rect;
-		var count = 0;
-
-		do {
-			count++;
-
-			if (count > 1000) {
-				console.log("Failed to place word");
-				return;
+		// Circle brush
+		for(var by = -10; by <= 10; by++) {
+			for(var bx = -10; bx <= 10; bx++) {
+				setParticle(x + bx, y + by, null);
 			}
-
-			x = Math.random() * boxRect.width;
-			y = Math.random() * boxRect.height;
-	
-			var distance = getDistance(center, { x: x, y: y });
-			rect = getRect(skills[i].word, x, y, fontsize);
-			offset(rect, 10, 10);
-		} while (distance > 300 || overlap(rect, placedWords, skills[i].type));
-		
-		placedWords.push({ rect: rect, type: skills[i].type });
-		placeWord(skills[i].word, x, y, fontsize);
-	}
-}
-
-var tr = 0;
-function initWordbox() {
-	tr++;
-	if(tr > 10) {
-		console.log("Failed to init wordbox");
-		return;
-	}
-	
-	var wordbox = document.getElementById("wordbox");
-	wordbox.innerHTML = "";
-	
-	var wordboxRect = wordbox.getBoundingClientRect();
-	var wordboxWidth = wordboxRect.width;
-	var wordboxHeight = wordboxRect.height;
-
-	var placedWords = [];
-
-	var success = true;
-
-	skills.forEach(skill => {
-		var x;
-		var y;
-
-		var size;
-
-		switch(skill.size) {
-			case 1:
-				size = 100;
-				break;
-			case 2:
-				size = 40;
-				break;
-			case 3:
-				size = 20;
-				break;
 		}
-
-		var rect = getRect(skill.word, 0, 0, size);
-
-		var count = 0;
-
-		do {
-			if (count++ > 1000) {
-				wordbox.innerHTML = "";
-				console.log("Failed to place word, retry");
-				success = false;
-				return false;
-			}
-			x = Math.random() * (wordboxWidth - rect.width);
-			y = Math.random() * (wordboxHeight - rect.height);
-
-			rect = getRect(skill.word, x, y, size);
-		} while (overlap(rect, placedWords, skill.type));
-
-		placedWords.push({rect: rect, type: skill.type});
-		placeWord(skill.word, x, y, size);
 	});
-
-	if(!success)
-		initWordbox();
 }
 
-function overlap(rect, rects, type) {
-	for (var i = 0; i < rects.length; i++) {
-		var other = rects[i].rect;
-		
-		if (rects[i].type == type) {
-			other = offset(other, word_offset_same.x, word_offset_same.y);
-		}
-		else {
-			other = offset(other, word_offset.x, word_offset.y);
-		}
+function initCanvas() {
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
 
-		if (rect.x < other.x + other.width &&
-			rect.x + rect.width > other.x &&
-			rect.y < other.y + other.height &&
-			rect.y + rect.height > other.y) {
-			return true;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	var arrayW = Math.floor(canvas.width / particleSize);
+	var arrayH = Math.floor(canvas.height / particleSize);
+
+	particles = new Array(arrayH);
+	for (var y = 0; y < arrayH; y++) {
+		particles[y] = new Array(arrayW);
+		particles[y].fill(null);
+	}
+	
+	setInterval(update, 1000 / 60);
+	setInterval(render, 1000 / 60);
+}
+
+function update() {
+	// Update Particles
+	for(var y = particles.length - 1; y >= 0; y--) {
+		for(var x = 0; x < particles[y].length; x++) {
+			if (getParticle(x, y) != null) {
+				// Gravity
+				if (getParticle(x, y + 1) == null) {
+					setParticle(x, y + 1, getParticle(x, y));
+					setParticle(x, y, null);
+				}
+				else {
+					if (getParticle(x - 1, y + 1) == null && getParticle(x - 1, y) == null) {
+						setParticle(x - 1, y + 1, getParticle(x, y));
+						setParticle(x, y, null);
+					}
+					if (getParticle(x + 1, y + 1) == null && getParticle(x + 1, y) == null) {
+						setParticle(x + 1, y + 1, getParticle(x, y));
+						setParticle(x, y, null);
+					}
+				}
+			}
 		}
 	}
-	return false;
 }
 
-function offset(rect, x, y) {
-	return {x: rect.x - x, y: rect.y - y, width: rect.width + x*2, height: rect.height + y*2};
+function render() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	for(var y = 0; y < particles.length; y++) {
+		for(var x = 0; x < particles[y].length; x++) {
+			if(getParticle(x, y) != null && getParticle(x, y) != undefined) {
+				ctx.fillStyle = particles[y][x].color;
+				ctx.fillRect(x * particleSize, y * particleSize, particleSize, particleSize);
+			}
+		}
+	}
 }
 
-function getRect(word, x, y, size) {
-	var wordElement = document.createElement("h3");
-	wordElement.innerHTML = word;
-	wordElement.style.fontSize = size + "px";
-	wordElement.style.position = "absolute";
-	wordElement.style.top = "-1000px";
-	wordElement.style.left = "-1000px";
-	document.body.appendChild(wordElement);
-	var rect = wordElement.getBoundingClientRect();
-	document.body.removeChild(wordElement);
-	return {x: x, y: y, width: rect.width, height: rect.height};
+function setParticle(x, y, particle) {
+	if(x < 0 || x >= particles[0].length || y < 0 || y >= particles.length) {
+		return -1;
+	}
+
+	particles[y][x] = particle;
 }
 
-function placeWord(word, x, y, size) {
-	var wordbox = document.getElementById("wordbox");
-	var wordElement = document.createElement("h3");
-	wordElement.innerHTML = word;
-	wordElement.style.fontSize = size + "px";
-	wordElement.style.top = y + "px";
-	wordElement.style.left = x + "px";
-	wordbox.appendChild(wordElement);
-	var rect = wordElement.getBoundingClientRect();
-	return {x: x, y: y, width: rect.width, height: rect.height};
+function getParticle(x, y) {
+	if(x < 0 || x >= particles[0].length || y < 0 || y >= particles.length) {
+		return -1;
+	}
+
+	return particles[y][x];
+}
+
+function spawnString(sx, sy, string) {
+	const colors = [
+		"#ff80ed",
+		"#ffd700",
+		"#fa8072",
+		"#66cdaa",
+		"#ff7f50"
+	];
+
+	if(sx + getStringWidth(string) > (canvas.width / particleSize)) {
+		sx -= getStringWidth(string) + 1;
+	}
+
+	var color = colors[Math.floor(Math.random() * colors.length)];
+
+	var width = 0;
+	for(var i = 0; i < string.length; i++) {
+		spawnChar(sx + width, sy + getGlyphOffset(string[i]), string[i], color);
+		width += getGlyphWidth(string[i]) + 1;
+	}
+}
+
+function getStringWidth(string) {
+	var width = 0;
+	for(var i = 0; i < string.length; i++) {
+		width += getGlyphWidth(string[i]) + 1;
+	}
+	return width;
+}
+
+function spawnChar(sx, sy, char, color) {
+	var data = fontData.glyphs[char].pixels;
+
+	for(var y = 0; y < data.length; y++) {
+		for(var x = 0; x < data[y].length; x++) {
+			if(data[y][x] == 1) {
+				spawn(sx + x, sy + y, color);
+			}
+		}
+	}
+}
+
+function getGlyphWidth(char) {
+	return fontData.glyphs[char].pixels[0].length;
+}
+
+function getGlyphOffset(char) {
+	return fontData.glyphs[char].offset;
+}
+
+function spawn(x, y, color) {
+	var particle = {
+		color: color
+	}
+
+	particles[y][x] = particle;
 }
 
 function initItems() {
@@ -252,7 +227,7 @@ function scrollAnimation() {
 		pageChange = true;
 	}
 
-	console.log(page, transition);
+	//console.log(page, transition);
 
 	/* Default */
 	document.getElementById("intro").style.opacity = 0;
@@ -383,20 +358,19 @@ function scrollAnimation() {
 		document.getElementById("skills-title").style.transform = "translateY(" + (-transition * 15) + "vh)";
 	}
 
-
 	/* Showing Skills */
 	if (page == 19) {
-		document.getElementById("skills").style.opacity = transition50up;
-		document.getElementById("skills").style.position = "relative";
-	}
-	
-	if(page == 20) {
-		document.getElementById("skills").style.position = "fixed";
-		document.getElementById("skills").style.top = "0px";
-		document.getElementById("skills").style.left = "0px";
 		document.getElementById("skills").style.opacity = 1;
-	}
 
+		if(spawnInterval != undefined)
+			clearInterval(spawnInterval);
+
+		var index = 0;
+		spawnInterval = setInterval(() => {
+			index++;
+			spawnString(Math.floor(Math.random() * (canvas.width / particleSize)), 0, words[index % words.length]);
+		}, 400);
+	}
 
 
 	/* Opacity Zero then Pause */
